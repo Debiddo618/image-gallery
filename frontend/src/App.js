a0e90400b9597b23a714edfc31f03bc46d638c6c
Debiddo <dzheng@terpmail.umd.edu>
2024-08-21 17:08:34 -0400

    saved images in the frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ImageCard from "./components/ImageCard";
 import { Container, Row, Col } from "react-bootstrap";
 import Welcome from "./components/Welcome";
@@ -14,6 +14,19 @@ const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5050";
 function App() {
   const [word, setWord] = useState("");
   const [images, setImages] = useState([]);
+
+  const getSavedImages = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/images`);
+      setImages(res.data || []);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getSavedImages();
+  }, []);
 
   const handleSearchSubmit = async (e) => {
     e.preventDefault();
@@ -31,6 +44,24 @@ function App() {
     setImages(images.filter((image) => image.id !== id));
   };
 
+  const handleSaveImage = async (id) => {
+    const imageToBeSaved = images.find((image) => image.id === id);
+    imageToBeSaved.saved = true;
+
+    try {
+      const res = await axios.post(`${API_URL}/images`, imageToBeSaved);
+      if (res.data?.inserted_id) {
+        setImages(
+          images.map((image) =>
+            image.id === id ? { ...image, saved: true } : image
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <Header title="Images Gallery" />
@@ -40,7 +71,11 @@ function App() {
           <Row xs={1} md={2} lg={3}>
             {images.map((image, index) => (
               <Col key={index} className="pb-3">
-                <ImageCard image={image} deleteImage={handleDeleteImage} />
+                <ImageCard
+                  image={image}
+                  deleteImage={handleDeleteImage}
+                  saveImage={handleSaveImage}
+                />
               </Col>
             ))}
           </Row>
--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,16 +1,25 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-function ImageCard({image, deleteImage}) {
+function ImageCard({ image, deleteImage, saveImage }) {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={image.urls.small} />
       <Card.Body>
-        <Card.Title>{image.title.toUpperCase()}</Card.Title>
-        <Card.Text>
-            {image.description || image.alt_description}
-        </Card.Text>
-        <Button variant="primary" onClick={()=>deleteImage(image.id)}>Delete</Button>
+        <Card.Title>{image.title?.toUpperCase()}</Card.Title>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant="primary" onClick={() => deleteImage(image.id)}>
+          Delete
+        </Button>
+        {!image.saved && (
+          <Button
+            className="mx-1"
+            variant="secondary"
+            onClick={() => saveImage(image.id)}
+          >
+            Save
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
